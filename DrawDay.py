2d6b65b2b06f9c6c9411d513d15d7e8d3e4c2995
CAccampo <carmenaccampo@gmail.com>
2024-11-10 17:04:06 -0600

    Prog: reply with streak; test reply

--- DrawDay.py
+++ DrawDay.py
@@ -5,29 +5,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('GUILD_ID')
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-guild = client.get_guild(GUILD)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
 
 @client.event
 async def on_message(message):
     channel = message.channel
     if message.author == client.user:
         return
-
-    # nickname = message.author.nick
-    # print(nickname)
-    # await message.author.edit(nick='1')
+    '''
+    TO DO:  
+            keep up with each user's streak
+            only check first image of day
+            cancellation with react
+    '''
     for att in message.attachments:
         if att.content_type.startswith('image'):
             await message.add_reaction('✅')
+            await message.reply(f'Your streak is {0}. React here with ❌ to cancel')
 
 client.run(TOKEN)
